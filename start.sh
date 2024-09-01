ae96731419d5dc34ad9e3f17375e721cf13eb647
Shengcong.Lin <linshengcong@foxmail.com>
2024-09-01 21:40:21 +0800

    feat: 随笔

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-required_version="v14.19.0"
+target_version="v14.21.3"
 
 current_version=$(node -v)
 
-echo "目前系统node 版本是$current_version, 需要依赖的node 版本$required_version"
+echo "目前系统node 版本是$current_version, 需要依赖的node 版本$target_version"
 
-if [ ${current_version} != ${required_version} ]; then
-  n '14.19.0'
+if [ ${current_version} != ${target_version} ]; then
+  nvm use '14.21.3'
 fi
 
 npx vuepress dev docs
